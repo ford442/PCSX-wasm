7f8f2478fb62a22474d403a22a367dcdce5ea9c3
Noah Cohn <noahc42@gmail.com>
2021-10-31 22:26:34 -0700

    Update sdl.c

--- plugins/dfsound/sdl.c
+++ plugins/dfsound/sdl.c
@@ -74,7 +74,7 @@ void SetupSound(void) {
 	InitSDL();
 
 	spec.freq = 44100;
-	spec.format = AUDIO_U16MSB;
+	spec.format = AUDIO_U16SYS;
 	spec.channels = 2;
 	spec.samples = 512;
 	spec.callback = SOUND_FillAudio;
