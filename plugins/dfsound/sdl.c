11945f89c0a6e1b53f7cf38a88b6d17a35eda723
Noah Cohn <noahc42@gmail.com>
2021-11-01 13:36:56 -0700

    Update sdl.c

--- plugins/dfsound/sdl.c
+++ plugins/dfsound/sdl.c
@@ -75,7 +75,7 @@ void SetupSound(void) {
 
 	spec.freq = 44100;
 	spec.format = AUDIO_S16SYS;
-	spec.channels = 2;
+	spec.channels = iDisStereo ? 1 : 2;
 	spec.samples = 512;
 	spec.callback = SOUND_FillAudio;
 
@@ -85,7 +85,7 @@ void SetupSound(void) {
 	}
 
 	iBufSize = BUFFER_SIZE;
-	iBufSize /= 2;
+	if (iDisStereo) iBufSize /= 2;
 
 	pSndBuffer = (short *)malloc(iBufSize * sizeof(short));
 	if (pSndBuffer == NULL) {
