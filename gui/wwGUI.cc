cf39c68ab4de3b3efe2a6d98e4ad80d27569fd52
Noah Cohn <noahc42@gmail.com>
2022-01-25 14:55:17 -0800

    Update wwGUI.cc

--- gui/wwGUI.cc
+++ gui/wwGUI.cc
@@ -1,5 +1,4 @@
 #include <emscripten/emscripten.h>
-#include <emscripten/html5.h>
 #include <stdlib.h>
 #include <SDL/SDL.h>
 #include <pthread.h>
@@ -106,8 +105,8 @@ int main()
   else
   {
     printf("sdl init ok\n");
-    sdl_display = SDL_SetVideoMode(640, 480, 32, 0);
-    sdl_ximage = SDL_CreateRGBSurface(0, 640, 480, 32, 0x00ff0000, 0x0000ff00, 0x000000ff, 0);
+    sdl_display = SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE);
+    sdl_ximage = SDL_CreateRGBSurface(SDL_HWSURFACE  | SDL_NOFRAME, 640, 480, 32, 0x00ff0000, 0x0000ff00, 0x000000ff, 0);
   }
   psxVuw = (unsigned short *)psxVub;
   SetupSound();
