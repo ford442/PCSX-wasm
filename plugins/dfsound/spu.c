4238ce1d84a39f6736e05047a2f31eeb74b82291
Noah Cohn <noahc42@gmail.com>
2022-02-05 15:43:15 -0800

    Update spu.c

--- plugins/dfsound/spu.c
+++ plugins/dfsound/spu.c
@@ -71,8 +71,8 @@ int             iUseTimer=2;
 int             iSPUIRQWait=1;
 int             iSoundDebugMode=0;
 int             iRecordMode=0;
-int             iUseReverb=0;
-int             iUseInterpolation=2;
+int             iUseReverb=1;
+int             iUseInterpolation=1;
 int             iDisStereo=0;
 
 // MAIN infos struct for each channel
