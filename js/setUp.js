4f453bc7eddf7b94f3236f9b06cfe07ac1d82305
Noah Cohn <noahc42@gmail.com>
2022-02-05 09:43:28 -0800

    Create setUp.js

---
+++ js/setUp.js
@@ -0,0 +1,10 @@
+function opn(){
+setTimeout(function(){
+document.getElementById('shut').innerHTML=2;
+document.getElementById("circle").width=window.innerWidth;
+document.getElementById("circle").height=window.innerHeight;
+let $hg=window.innerHeight+"px";
+document.getElementById("wrap").style.lineheight=$hg;
+document.getElementById("di").click();
+},500);
+};
