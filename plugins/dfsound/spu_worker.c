655437ed305010c872a7ce547c98f243e3663639
Noah Cohn <noahc42@gmail.com>
2022-02-09 04:17:04 -0800

    Update spu_worker.c

--- plugins/dfsound/spu_worker.c
+++ plugins/dfsound/spu_worker.c
@@ -71,7 +71,7 @@ int             iSoundDebugMode=0;
 int             iRecordMode=0;
 int             iUseReverb=2;
 int             iUseInterpolation=2;
-int             iDisStereo=1;
+int             iDisStereo=0;
 
 // MAIN infos struct for each channel
 
