d85b6fc1bfe24a5a3ebba3ce674a8eeeb4be3b95
Noah Cohn <noahc42@gmail.com>
2022-02-12 21:36:59 -0800

    Update worker.c

--- plugins/dfsound/worker.c
+++ plugins/dfsound/worker.c
@@ -21,8 +21,8 @@
 #include "externals.h"
 #include <emscripten.h>
 
-//#define BUFFER_SIZE		22050
-#define BUFFER_SIZE		44100
+#define BUFFER_SIZE		22050
+// #define BUFFER_SIZE		44100
 
 void SetupSound(void) {
 }
